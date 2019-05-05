671a18e623e8957f9eafedd2dfef82d21eac2309
Sam Thomas <samspot@gmail.com>
2019-05-05 08:47:46 -0400

    disable debug

--- src/config.ts
+++ src/config.ts
@@ -38,7 +38,7 @@ const Config: IConfig = {
     // seed: 12458,
     drawWholeMap: false,
     drawAllMobs: false,
-    debug: true,
+    debug: false,
     skipTitle: true,
     tiles: true,
     enableCharge: false,
