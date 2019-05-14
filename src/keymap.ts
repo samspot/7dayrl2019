966b9068c8b699c8302838230e55a74c34de1b0f
Sam Thomas <samspot@gmail.com>
2019-05-13 20:24:44 -0400

    fix keymap ignore

--- src/keymap.ts
+++ src/keymap.ts
@@ -1,6 +1,7 @@
-// @ts-ignore
-import { KEYS } from 'rot-js'
-
+import { VK_0, VK_1, VK_2, VK_3, VK_4, VK_5, VK_6, VK_7, VK_8, VK_9 } from "rot-js";
+import { VK_H, VK_A, VK_K, VK_W, VK_L, VK_D, VK_J, VK_S } from "rot-js";
+import { VK_LEFT, VK_UP, VK_RIGHT, VK_DOWN } from "rot-js";
+import { VK_Q, VK_E, VK_R } from "rot-js";
 
 // https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 
@@ -19,41 +20,41 @@ keyMap[35] = 5;
 keyMap[37] = 6;
 keyMap[36] = 7;
 
-keyMap[KEYS.VK_LEFT] = 6 // 37, left arrow
-keyMap[KEYS.VK_H] = 6 // 37, left arrow
-keyMap[KEYS.VK_A] = 6 // 37, left arrow
+keyMap[VK_LEFT] = 6 // 37, left arrow
+keyMap[VK_H] = 6 // 37, left arrow
+keyMap[VK_A] = 6 // 37, left arrow
 
-keyMap[KEYS.VK_UP] = 0; // up arrow
-keyMap[KEYS.VK_K] = 0; // up arrow
-keyMap[KEYS.VK_W] = 0; // up arrow
+keyMap[VK_UP] = 0; // up arrow
+keyMap[VK_K] = 0; // up arrow
+keyMap[VK_W] = 0; // up arrow
 
-keyMap[KEYS.VK_RIGHT] = 2; // right arrow
-keyMap[KEYS.VK_L] = 2; // right arrow
-keyMap[KEYS.VK_D] = 2; // right arrow
+keyMap[VK_RIGHT] = 2; // right arrow
+keyMap[VK_L] = 2; // right arrow
+keyMap[VK_D] = 2; // right arrow
 
-keyMap[KEYS.VK_DOWN] = 4; // down arrow
-keyMap[KEYS.VK_J] = 4; // down arrow 
-keyMap[KEYS.VK_S] = 4; // down arrow 
+keyMap[VK_DOWN] = 4; // down arrow
+keyMap[VK_J] = 4; // down arrow 
+keyMap[VK_S] = 4; // down arrow 
 
 export const numberKeys = [
-    KEYS.VK_0,
-    KEYS.VK_1,
-    KEYS.VK_2,
-    KEYS.VK_3,
-    KEYS.VK_4,
-    KEYS.VK_5,
-    KEYS.VK_6,
-    KEYS.VK_7,
-    KEYS.VK_8,
-    KEYS.VK_9
+    VK_0,
+    VK_1,
+    VK_2,
+    VK_3,
+    VK_4,
+    VK_5,
+    VK_6,
+    VK_7,
+    VK_8,
+    VK_9
 ]
 
 export function isEscKey(charCode: number) {
-    return charCode === 27 || charCode === KEYS.VK_Q || charCode === KEYS.VK_E || charCode === KEYS.VK_R
+    return charCode === 27 || charCode === VK_Q || charCode === VK_E || charCode === VK_R
 }
 
 export const abilitykeys = [
-    KEYS.VK_Q,
-    KEYS.VK_E,
-    KEYS.VK_R
+    VK_Q,
+    VK_E,
+    VK_R
 ]
